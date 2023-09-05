d456be04ce90024f9fe7eddec968f9292342e7f2
X2Abdul <arehman000935@gmail.com>
2023-09-05 04:19:57 +0100

    react setup - done project foundation - done

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+import './index.css'
+
+export default function App(){
+  
+  return(
+    <>
+      <form className='new-item-form'>
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input type="text" id='item' />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">Todo List</h1>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" />
+            item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+    </>
+  )
+}
